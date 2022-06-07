6b505d96c83acb9ec34c74fc34fa9cf49acde33b
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-06-06 20:24:02 -0400

    Open index.ifc and read a given expressID from it; disable testing for now.

--- config/esbuild.js
+++ config/esbuild.js
@@ -10,19 +10,10 @@ const build = {
   // https://esbuild.github.io/api/#keep-names
   // We use code identifiers e.g. in ItemProperties for their names
   keepNames: true,
-  // Splitting
-  // Entry points (our src/index.jsx) are currently not named with
-  // cache-busting segments, like index-x84nfi.js, so we should be
-  // careful with our caching, i.e. not putting much index.jsx.
-  // See:
-  //   https://esbuild.github.io/api/#chunk-names
-  //   https://github.com/evanw/esbuild/issues/16
-  splitting: false,
-  outdir: buildDir,
-  format: 'esm',
   sourcemap: true,
+  outdir: buildDir,
+  platform: 'node',
   target: ['node14'],
-  logLevel: 'info',
 }
 
 esbuild
--- src/ifctool.js
+++ src/ifctool.js
@@ -1,11 +1,29 @@
+import WebIFC from 'web-ifc'
+import fs from 'fs'
+
+
+async function readIfc(expressId) {
+  const ifcApi = new WebIFC.IfcAPI();
+  await ifcApi.Init();
+  const rawFileData = fs.readFileSync('index.ifc')
+  let modelID = ifcApi.OpenModel(
+    rawFileData,
+    /* optional settings object */
+  );
+  console.log(`line ${expressId}: `, ifcApi.GetLine(modelID, expressId, true))
+  ifcApi.CloseModel(modelID);
+}
+
+
 /**
  * @return {string} Message
  */
-function main() {
+export function main() {
+  const args = process.argv.slice(2)
+  console.log('args: ', args)
+  readIfc(args[0] != undefined ? parseInt(args[0]) : 0)
   return 'Hello IFC!'
 }
 
-module.exports = {main}
 
-
-console.log(main())
+main()
