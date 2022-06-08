22bbd339444ba95a085d499997afb23bac3c452e
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-06-08 06:02:31 -0400

    On --type match ignore case

--- src/ifctool.js
+++ src/ifctool.js
@@ -57,7 +57,7 @@ export async function main() {
   if (flags.id) {
     ret = model.getElt(parseInt(flags.id))
   } else if (flags.type) {
-    ret = model.getEltsOfNamedType(flags.type)
+    ret = model.getEltsOfNamedType(flags.type.toUpperCase())
   }
   if (flags.out && flags.out == 'csv') {
     if (flags.fmt != undefined) {
