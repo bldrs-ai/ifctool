ef6626feb14c829f61852fce4edee3dd3a81d0f4
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-07-06 17:41:59 +0200

    Fix version reporting bug.  Published version 4.1.2 as latest.

--- src/version.js
+++ src/version.js
@@ -1,9 +1,4 @@
-import fs from 'fs'
-
-
 /** @return {string} version string from package.json */
 export function getPackageVersion() {
-  const rawFileData = fs.readFileSync('./package.json')
-  const pkgJson = JSON.parse(rawFileData)
-  return pkgJson.version
+  return '4.1.2' // VERSION
 }
