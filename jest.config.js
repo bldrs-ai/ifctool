1a467f75d6f37595ec48a21772ee616a0f32f9f6
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-08-30 15:51:36 -0400

    Introduce Pipeline to configure and run plugins.  Connect level extraction tools as plugins.

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  verbose: false,
+  projects: [
+    {
+      displayName: 'ifclib',
+      testMatch: ['<rootDir>/lib/src/**/*.test.js'],
+      testEnvironment: 'node',
+      testPathIgnorePatterns: [],
+      transform: {
+        '\\.js?$': 'babel-jest',
+      },
+      transformIgnorePatterns: [
+        '/node_modules/(?!(web-ifc)/)',
+      ],
+      moduleNameMapper: {},
+      setupFilesAfterEnv: [],
+    },
+  ],
+}
--- tool/src/main.js
+++ tool/src/main.js
@@ -69,8 +69,8 @@ import fs from 'fs'
 import log4js from 'log4js'
 import 'log4js/lib/appenders/stderr.js'
 import {parseFlags} from './flags.js'
-//import {Exception, logLevels, processIfcBuffer, setLogLevel} from '@bldrs-ai/ifclib'
-import {Exception, logLevels, processIfcBuffer, setLogLevel} from '../../lib/src/index.js'
+import {Exception, logLevels, processIfcBuffer, setLogLevel} from '@bldrs-ai/ifclib'
+// import {Exception, logLevels, processIfcBuffer, setLogLevel} from '../../lib/src/index.js'
 
 
 log4js.configure({
@@ -110,6 +110,9 @@ export async function processArgs(args, print=console.log) {
       }
     }
     const flags = parseFlags(args)
+    flags.__meta = {
+      inputFilename: ifcFilename
+    }
     if (flags.log) {
       const logLevel = flags.log
       if (!logLevels.includes(logLevel)) {
